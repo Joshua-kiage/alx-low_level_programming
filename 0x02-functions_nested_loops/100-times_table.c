3008af223653f8b3469593f69218af33f45dd1a5
Joshua-Kiage <joshkiage64@gmail.com>
2023-02-22 02:57:25 -0800

    The World looks like a multiplication-table, or a mathematical equation, which, turn it how you will, balances itself

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,42 +1,47 @@
 #include "main.h"
-
 /**
- * print_times_table - Prints the times table of the input,
- *                     starting with 0.
- * @n: The value of the times table to be printed.
+ * print_times_table - print the n times table, from 0
+ * @n: if n < 0 && n > 15, stop printing
+ * Return: Always 0
  */
 void print_times_table(int n)
 {
-	int num, mult, prod;
+	int a, b, c;
 
-	if (n >= 0 && n <= 15)
+	if (n >= 0 && n <= 14)
 	{
-		for (num = 0; num <= n; num++)
+		for (a = 0; a <= n; a++)
 		{
-			_putchar('0');
-
-			for (mult = 1; mult <= n; mult++)
+			for (b = 0; b <= n; b++)
 			{
-				_putchar(',');
-				_putchar(' ');
-
-				prod = num * mult;
-
-				if (prod <= 99)
-					_putchar(' ');
-				if (prod <= 9)
-					_putchar(' ');
-
-				if (prod >= 100)
+				c = a * b;
+				if (c > 99)
 				{
-					_putchar((prod / 100) + '0');
-					_putchar(((prod / 10)) % 10 + '0');
+					_putchar(',');
+					_putchar(32);
+					_putchar((c / 100) + '0');
+					_putchar(((c / 10) % 10) + '0');
+					_putchar((c % 10) + '0');
 				}
-				else if (prod <= 99 && prod >= 10)
+				else if (c > 9)
 				{
-					_putchar((prod / 10) + '0');
+					_putchar(',');
+					_putchar(32);
+					_putchar(32);
+					_putchar(((c / 10) % 10) + '0');
+					_putchar((c % 10) + '0');
 				}
-				_putchar((prod % 10) + '0');
+				else
+				{
+					if (b != 0)
+					{
+						_putchar(',');
+						_putchar(32);
+						_putchar(32);
+						_putchar(32);
+					}
+					_putchar(c + '0');
+				}
 			}
 			_putchar('\n');
 		}
