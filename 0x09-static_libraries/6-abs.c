038d8224a37ce197a706d433cce8bf10b2a57c42
Joshua-Kiage <joshkiage64@gmail.com>
2023-03-10 05:42:31 -0800

    A library is not a luxury but one of the necessities of life

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @ab: The integer to be computed
+ * Return: The absolute value
+ */
+int _abs(int ab)
+{
+	if (ab >= 0)
+		return (ab);
+	else
+		return (-ab);
+}
