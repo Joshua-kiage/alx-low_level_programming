67bd596d3813333b0be086e7d43ab6d2a46ff970
Joshua-Kiage <joshkiage64@gmail.com>
2023-03-16 07:24:34 -0700

    Trust no one

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -5,15 +5,15 @@
  * *malloc_checked -  allocates memory using malloc.
  * @b: number of bytes to allocate
  * Return: a ptr to the allocated memory
+ * if malloc fails, status value is equal to EXIT_FAILURE
  */
 
 void *malloc_checked(unsigned int b)
 {
-	void *ptr = malloc(b);
+	char *ptr;
 
+	*ptr = malloc(b);
 	if (ptr == NULL)
-	{
 		exit(EXIT_FAILURE);
-	}
 	return (ptr);
 }
