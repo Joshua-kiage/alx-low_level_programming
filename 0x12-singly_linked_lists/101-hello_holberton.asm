e07c6feb0365b8b68888c41a35a93ed4f0e5d092
Josh-Kiage <joshkiage64@gmail.com>
2023-03-30 09:57:21 -0700

    6. Real programmers can write assembly code in any language

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,21 @@
+; My comments: It is so fun to write in assembly language
+; File: 101-hello_holberton.asm
+; Auth: Joshua Kiage
+; Desc: 64-bit assembly program that prints
+;       Hello, Holberton followed by a new line.
+
+SECTION .data
+msg:	db "Hello, Holberton", 0
+fmt:	db "%s", 10, 0
+
+	SECTION .text
+	extern printf
+	global main
+main:
+	mov esi, msg
+	mov edi, fmt
+	mov eax, 0
+	call printf
+
+	mov eax, 0
+	ret
