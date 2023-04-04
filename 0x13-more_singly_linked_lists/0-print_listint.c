1566ba9e180968088a62712ee34aae88a1bb5a06
Josh-Kiage <joshkiage64@gmail.com>
2023-04-04 12:40:30 -0700

    0. Print list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * print_listint - prints all the elements of a linked list
+ * print_listint -  prints all the elements of a listint_t list
  * @h: linked list of type listint_t to print
  * Return: the number of nodes
  */
