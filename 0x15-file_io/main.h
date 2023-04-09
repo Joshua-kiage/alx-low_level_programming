195e717c36212911ef006d4183c58b1ded8df257
Josh-Kiage <joshkiage64@gmail.com>
2023-04-09 06:57:14 -0700

    header file

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+void print_error_and_exit(const char *msg, const char *filename, int exit_code);
+void copy_file(const char *file_from, const char *file_to);
+void display_elf_header(const char *filename);
+
+#endif
