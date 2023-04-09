ffeb8ba86705a4acd1414c63665c5f6ffbf7656a
Josh-Kiage <joshkiage64@gmail.com>
2023-04-09 12:04:35 -0700

    0. Tread lightly, she is near

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * read_textfile - reads a text file and prints it
@@ -15,20 +17,20 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t num_read, num_write;
 	char *buf;
 
-	if (!filename)
+	if (filename == NULL)
+		return (0);
+
+	buf = malloc(sizeof(char) * letters);
+
+	if (buf == NULL)
 		return (0);
 
 	fd = open(filename, O_RDONLY);
-
-	if (fd == -1)
-		return (0);
-
-	buf = malloc(sizeof(char) * (letters));
-	if (!buf)
-		return (0);
-
 	num_read = read(fd, buf, letters);
 	num_write = write(STDOUT_FILENO, buf, num_read);
+
+	if (fd == -1 || num_read == -1 || num_write == -1 || num_write != num_read)
+		return (0);
 
 	close(fd);
 
