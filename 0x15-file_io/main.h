92353ec927d382d6835fa1dcaf98b1dbe924f495
Josh-Kiage <joshkiage64@gmail.com>
2023-04-10 10:34:49 -0700

    header file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -12,8 +12,5 @@ int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-void print_error_and_exit(const char *msg, const char *filename, int exit_code);
-void copy_file(const char *file_from, const char *file_to);
-void display_elf_header(const char *filename);
 
 #endif
