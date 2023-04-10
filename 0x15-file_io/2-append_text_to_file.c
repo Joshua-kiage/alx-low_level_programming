4167e2751d880cb660f4a4dcac3b9545f6c01e14
Josh-Kiage <joshkiage64@gmail.com>
2023-04-10 16:02:45 -0700

    2. Speak gently, she can hear

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	FILE *file;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+
+	file = fopen(filename, "a");
+
+	if (file == NULL)
+	{
+		return (-1);
+	}
+	if (text_content != NULL)
+	{
+		size_t len = strlen(text_content);
+
+		fwrite(text_content, sizeof(char), len, file);
+	}
+	fclose(file);
+	return (1);
+}
