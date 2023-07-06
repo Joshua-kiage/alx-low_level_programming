f25da0c6999d46db3a50795a7306ba9937e9543b
root <Joshua-kiage>
2023-07-06 10:20:17 -0700

    0. >>> ht = {}

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,7 +8,6 @@
  * Return: Pointer to the newly created hash table
  *         NULL if something went wrong
  */
-
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash_table;
